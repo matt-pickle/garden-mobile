2d437d494cf9d937c03f5475313cdd1f6eb7e6ba
Matt <mpickle86@gmail.com>
2021-09-09 21:40:48 -0500

    Add plantData and basic schedule creation

--- components/Schedule.js
+++ components/Schedule.js
@@ -3,26 +3,50 @@ import {Text, View, TouchableOpacity} from "react-native";
 import {Picker} from "@react-native-picker/picker";
 import uuid from "react-native-uuid";
 import { Ionicons } from '@expo/vector-icons';
+import plantData from "../database/plantData";
 
 function Schedule(props) {
-  const [plantedArr, setPlantedArr] = useState([]);
 
-  useEffect(() => {
-      let combinedPlantedArr = [];
-      props.gardens.forEach(item => {
-        combinedPlantedArr = combinedPlantedArr.concat(item.plantedArr);
-      });
-      combinedPlantedArr.sort();
-      const filteredPlantedArr = combinedPlantedArr.filter((item, index) => {
-        return item !== "none" && item !== combinedPlantedArr[index - 1];
-      });
-      setPlantedArr(filteredPlantedArr);
-  }, []);
+  let combinedPlantedArr = [];
+  props.gardens.forEach(item => {
+    combinedPlantedArr = combinedPlantedArr.concat(item.plantedArr);
+  });
+  combinedPlantedArr.sort();
+  const plantNamesArr = combinedPlantedArr.filter((item, index) => {
+    return item !== "none" && item !== combinedPlantedArr[index - 1];
+  });
+  const plantObjsArr = plantData.filter(item => {
+    return plantNamesArr.includes(item.name);
+  });
+
+  let datesArr = [];
+  plantObjsArr.forEach(item => {
+    if (item.transplantDate) {
+      datesArr = datesArr.concat([
+        {[item.startDate]: item.startMessage},
+        {[item.transplantDate]: item.transplantMessage},
+        {[item.harvestDate]: item.harvestMessage}
+      ]);
+    } else {
+      datesArr = datesArr.concat([
+        {[item.startDate]: item.startMessage},
+        {[item.harvestDate]: item.harvestMessage}
+      ]);
+    }
+  });
+
+  datesArr.sort((a, b) => {
+    return Object.keys(a) - Object.keys(b);
+  });
+
+  const scheduleArr = datesArr.map((item, index) => {
+    return <Text key={index}>{Object.keys(item)}: {Object.values(item)}</Text>
+  });
 
   return (
     <View>
       <Text>Schedule</Text>
-      <Text>{plantedArr}</Text>
+      {scheduleArr}
       <TouchableOpacity
         onPress={() => props.setIsScheduleOpen(false)}
       >
---
+++ database/plantData.js
@@ -0,0 +1,23 @@
+export default plantData = [
+  {
+    name: "broccoli",
+    perSquareFoot: 1,
+    startDate: 1,
+    startMessage: "Start broccoli seeds indoors",
+    transplantDate: 20,
+    transplantMessage: "Transplant broccoli to garden",
+    harvestDate: 100,
+    harvestMessage: "Harvest broccoli when head forms, before it starts to bloom"
+  },
+
+  {
+    name: "onion",
+    perSquareFoot: 4,
+    startDate: 2,
+    startMessage: "Start onion seeds indoors",
+    transplantDate: 21,
+    transplantMessage: "Transplant onions to garden",
+    harvestDate: 101,
+    harvestMessage: "Harvest onions when tops dry out and fall over"
+  }
+];
