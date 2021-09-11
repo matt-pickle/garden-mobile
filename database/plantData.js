3f703575edf76f882f0e109b380b9a44dd47e606
Matt <mpickle86@gmail.com>
2021-09-11 17:32:27 -0500

    Add frostDateData and update plantData

--- database/plantData.js
+++ database/plantData.js
@@ -2,22 +2,22 @@ export default plantData = [
   {
     name: "broccoli",
     perSquareFoot: 1,
-    startDate: 1,
+    startDate: -84,
     startMessage: "Start broccoli seeds indoors",
-    transplantDate: 20,
+    transplantDate: -35,
     transplantMessage: "Transplant broccoli to garden",
-    harvestDate: 100,
+    harvestDate: 28,
     harvestMessage: "Harvest broccoli when head forms, before it starts to bloom"
   },
 
   {
     name: "onion",
     perSquareFoot: 4,
-    startDate: 2,
+    startDate: -70,
     startMessage: "Start onion seeds indoors",
-    transplantDate: 21,
+    transplantDate: -28,
     transplantMessage: "Transplant onions to garden",
-    harvestDate: 101,
+    harvestDate: 70,
     harvestMessage: "Harvest onions when tops dry out and fall over"
   }
 ];
