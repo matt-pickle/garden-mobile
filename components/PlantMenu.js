22d348a80b7d5572e993f3024faede89ed9308c5
Matt Pickle <mpickle86@gmail.com>
2022-01-08 23:31:47 +0000

    Move icon picking logic to separate js file

--- components/PlantMenu.js
+++ components/PlantMenu.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import {View, ScrollView, Text, Image, TouchableOpacity, Dimensions} from "react-native";
 import plantData from "../database/plantData.js";
+import iconPicker from "../functions/iconPicker";
 
 function PlantMenu(props) {
   let isMenuHorizontal = true;
@@ -14,69 +15,7 @@ function PlantMenu(props) {
   });
 
   const menu = plantableArr.map((item, index) => {
-    let icon = require("../assets/plant-icons/none.png");
-    switch (item.name) {
-      case "bell-pepper":
-        icon = require("../assets/plant-icons/bell-pepper.png");
-        break;
-      case "broccoli":
-        icon = require("../assets/plant-icons/broccoli.png");
-        break;
-      case "cabbage":
-        icon = require("../assets/plant-icons/cabbage.png");
-        break;
-      case "carrot":
-        icon = require("../assets/plant-icons/carrot.png");
-        break;
-      case "cauliflower":
-        icon = require("../assets/plant-icons/cauliflower.png");
-        break;
-      case "cucumber":
-        icon = require("../assets/plant-icons/cucumber.png");
-        break;
-      case "green bean":
-        icon = require("../assets/plant-icons/green-bean.png");
-        break;
-      case "jalapeno":
-        icon = require("../assets/plant-icons/jalapeno.png");
-        break;
-      case "kale":
-        icon = require("../assets/plant-icons/kale.png");
-        break;
-      case "lettuce":
-        icon = require("../assets/plant-icons/lettuce.png");
-        break;
-      case "onion":
-        icon = require("../assets/plant-icons/onion.png");
-        break;
-      case "okra":
-        icon = require("../assets/plant-icons/okra.png");
-        break;
-      case "potato":
-        icon = require("../assets/plant-icons/potato.png");
-        break;
-      case "radish":
-        icon = require("../assets/plant-icons/radish.png");
-        break;
-      case "spinach":
-        icon = require("../assets/plant-icons/spinach.png");
-        break;
-      case "sugar snap pea":
-        icon = require("../assets/plant-icons/sugar-snap-pea.png");
-        break;
-      case "sweet pea":
-        icon = require("../assets/plant-icons/sweet-pea.png");
-        break;
-      case "sweet potato":
-        icon = require("../assets/plant-icons/sweet-potato.png");
-        break;
-      case "tomato":
-        icon = require("../assets/plant-icons/tomato.png");
-        break;
-      case "turnip":
-        icon = require("../assets/plant-icons/turnip.png");
-        break;
-    }
+    const icon = iconPicker(item.name);
 
     return (
       <TouchableOpacity
--- components/Square.js
+++ components/Square.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {Image, TouchableOpacity} from "react-native";
+import iconPicker from "../functions/iconPicker";
 
 function Square(props) {
   const [planted, setPlanted] = useState(props.planted);
@@ -9,69 +10,7 @@ function Square(props) {
     props.changePlantInArr(props.id, props.selectedPlant);
   }
 
-  let icon = require("../assets/plant-icons/none.png");
-  switch (planted) {
-    case "bell-pepper":
-      icon = require("../assets/plant-icons/bell-pepper.png");
-      break;
-    case "broccoli":
-      icon = require("../assets/plant-icons/broccoli.png");
-      break;
-    case "cabbage":
-      icon = require("../assets/plant-icons/cabbage.png");
-      break;
-    case "carrot":
-      icon = require("../assets/plant-icons/carrot.png");
-      break;
-    case "cauliflower":
-      icon = require("../assets/plant-icons/cauliflower.png");
-      break;
-    case "cucumber":
-      icon = require("../assets/plant-icons/cucumber.png");
-      break;
-    case "green bean":
-      icon = require("../assets/plant-icons/green-bean.png");
-      break;
-    case "jalapeno":
-      icon = require("../assets/plant-icons/jalapeno.png");
-      break;
-    case "kale":
-      icon = require("../assets/plant-icons/kale.png");
-      break;
-    case "lettuce":
-      icon = require("../assets/plant-icons/lettuce.png");
-      break;
-    case "onion":
-      icon = require("../assets/plant-icons/onion.png");
-      break;
-    case "okra":
-      icon = require("../assets/plant-icons/okra.png");
-      break;
-    case "potato":
-      icon = require("../assets/plant-icons/potato.png");
-      break;
-    case "radish":
-      icon = require("../assets/plant-icons/radish.png");
-      break;
-    case "spinach":
-      icon = require("../assets/plant-icons/spinach.png");
-      break;
-    case "sugar snap pea":
-      icon = require("../assets/plant-icons/sugar-snap-pea.png");
-      break;
-    case "sweet pea":
-      icon = require("../assets/plant-icons/sweet-pea.png");
-      break;
-    case "sweet potato":
-      icon = require("../assets/plant-icons/sweet-potato.png");
-      break;
-    case "tomato":
-      icon = require("../assets/plant-icons/tomato.png");
-      break;
-    case "turnip":
-      icon = require("../assets/plant-icons/turnip.png");
-      break;
-  }
+  const icon = iconPicker(planted);
 
   return (
     <TouchableOpacity
