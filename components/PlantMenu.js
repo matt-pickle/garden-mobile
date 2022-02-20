a451ac581eb05d714f1adfa94b3f928ac9d78918
Matt <mpickle86@gmail.com>
2022-02-19 18:09:09 -0600

    Memoize plant menu

--- components/PlantMenu.js
+++ components/PlantMenu.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useMemo} from "react";
 import {View, ScrollView, Text, Image, TouchableOpacity, Dimensions} from "react-native";
 import plantData from "../database/plantData.js";
 import iconPicker from "../functions/iconPicker";
@@ -10,34 +10,42 @@ function PlantMenu(props) {
     isMenuHorizontal = false;
   }
 
-  const plantableArr = plantData.filter(item => {
-    return item.zones.includes(props.zone);
-  });
-
-  const menu = plantableArr.map((item, index) => {
-    const icon = iconPicker(item.name);
-
+  const plantableArr = useMemo(() => {
     return (
-      <TouchableOpacity
-        style={[props.styles.menuButton, (props.selectedPlant === item.name && props.styles.selectedPlantIcon)]}
-        onPress={() => props.setSelectedPlant(item.name)}
-        key={index}
-      >
-        <Image source={icon} style={{width: "100%", height: "100%"}}/>
-        <Text style={props.styles.menuButtonText}>{item.name}</Text>
-      </TouchableOpacity>
-    );
-  });
+      plantData.filter(item => {
+        return item.zones.includes(props.zone);
+      })
+    )
+  }, [])
+  
+  const menu = useMemo(() => {
+    return (
+      plantableArr.map((item, index) => {
+        const icon = iconPicker(item.name);
+        return (
+          <TouchableOpacity
+            style={[props.styles.menuButton, (props.selectedPlant === item.name && props.styles.selectedPlantIcon)]}
+            onPress={() => props.setSelectedPlant(item.name)}
+            key={index}
+          >
+            <Image source={icon} style={{width: "100%", height: "100%"}}/>
+            <Text style={props.styles.menuButtonText}>{item.name}</Text>
+          </TouchableOpacity>
+        );
+      })
+    )
+  }, [plantableArr, props.selectedPlant])
+  
 
   return (
     <View style={props.styles.menuContainer}>
-        <ScrollView
-          horizontal={isMenuHorizontal}
-          contentContainerStyle={props.styles.contentContainer}
-          persistentScrollbar={true}
-        >
-          {menu}
-        </ScrollView>
+      <ScrollView
+        horizontal={isMenuHorizontal}
+        contentContainerStyle={props.styles.contentContainer}
+        persistentScrollbar={true}
+      >
+        {menu}
+      </ScrollView>
     </View>
   );
 }
